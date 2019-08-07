05303604f8d84671a7368d299a9685c96b48d599
Husni <husnijabir@gmail.com>
2019-08-07 21:00:42 +0530

    Restructured the code

---
+++ src/components/todoapplication.js
@@ -0,0 +1,90 @@
+import React from "react";
+import axios from "axios";
+
+export class TodoApplication extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      text: ""
+    };
+  }
+
+  render() {
+    const { items, text } = this.state;
+    return (
+      <div>
+        <h1>TODO</h1>
+        <form>
+          <label htmlFor="new-todo">What needs to be done?</label>
+          <br />
+          <input id="new-todo" onChange={this.handleChange} value={text} />
+          <br />
+          <button onClick={this.handleSubmit}>Add #{items.length + 1}</button>
+          {/* <button onClick={this.fetchData}>Get Data From API</button> */}
+          <TodoData text="Get Data" dataFunction={this.fetchData.bind(this)} />
+        </form>
+        <TodoList items={items} />
+      </div>
+    );
+  }
+
+  fetchData = e => {
+    e.preventDefault();
+    const { items } = this.state;
+    const usersCount = items.length + 1;
+    axios
+      .get("https://reqres.in/api/users/" + usersCount)
+      .then(function(response) {
+        // response.data.data.map(d => items.push({ id: d.id, text: d.last_name }));
+        items.push({
+          id: response.data.data.id,
+          text: response.data.data.last_name
+        });
+      });
+    this.setState({ items: items });
+  };
+
+  handleSubmit = e => {
+    const { items } = this.state;
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+
+    items.push({
+      text: this.state.text,
+      id: Date.now()
+    });
+
+    this.setState({
+      items: items,
+      text: ""
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+}
+
+const TodoList = props => {
+  return (
+    <ul>
+      {props.items.map(item => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+const TodoData = props => {
+  return (
+    <div>
+      <br />
+      <button onClick={props.dataFunction}>{props.text}</button>
+    </div>
+  );
+};
